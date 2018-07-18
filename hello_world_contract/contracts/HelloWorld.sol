ef952e7842173b648799948a7d87ce4b7462a542
pavel_macos <pavel.malygin@gmail.com>
2018-07-18 10:48:10 +0300

    test 1

---
+++ hello_world_contract/contracts/HelloWorld.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+contract HelloWorld {
+
+    string m_name;
+
+    constructor(string name) public {
+
+        m_name = name;
+    }
+
+    function hello() public view returns (string) {
+        return m_name;
+    }
+
+
+}
